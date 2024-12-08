fba671bd34d091b5fddfb41345f930b2220f4b4b
Seyoung Park <seyoung8239@gmail.com>
2024-12-08 20:03:02 +0900

    feat: 캔버스 사이즈 조정 로직 추가

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,11 @@ import { GameMap } from "./objects/map/Map";
 class App {
 	canvas;
 	ctx;
+
+	stageWidth = 0;
+	stageHeight = 0;
+	pixelRatio = 1;
+
 	map;
 
 	constructor() {
@@ -12,7 +17,21 @@ class App {
 
 		this.map = new GameMap();
 
+		window.addEventListener("resize", this.resize.bind(this), false);
+		this.resize();
+
 		requestAnimationFrame(this.animate.bind(this));
+	}
+
+	resize() {
+		this.stageWidth = document.body.clientWidth;
+		this.stageHeight = document.body.clientHeight;
+
+		this.canvas.width = this.stageWidth * this.pixelRatio;
+		this.canvas.height = this.stageHeight * this.pixelRatio;
+		this.ctx!.scale(this.pixelRatio, this.pixelRatio);
+
+		console.log("resize", this.canvas.width, this.canvas.height);
 	}
 
 	animate() {
