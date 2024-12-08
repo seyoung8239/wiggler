efe8af3f251740582f2ae884e95cbd02e840848c
Seyoung Park <seyoung8239@gmail.com>
2024-12-08 19:45:06 +0900

    feat: add Point class

--- src/common/Point.ts
+++ src/common/Point.ts
@@ -1,9 +1,28 @@
-export class Point {
-    x: number;
-    y: number;
+import { DIRECTION, type Direction } from "./direction/direction.constant";
 
-    constructor(x: number, y: number) {
-        this.x = x;
-        this.y = y;
-    }
+export class Point {
+	x: number;
+	y: number;
+
+	constructor(x: number, y: number) {
+		this.x = x;
+		this.y = y;
+	}
+
+	static getMovedPoint(point: Point, direction: Direction) {
+		const { x, y } = point;
+		switch (direction) {
+			case DIRECTION.UP:
+				return new Point(x, y + 1);
+
+			case DIRECTION.DOWN:
+				return new Point(x, y - 1);
+
+			case DIRECTION.RIGHT:
+				return new Point(x - 1, y);
+
+			case DIRECTION.LEFT:
+				return new Point(x + 1, y);
+		}
+	}
 }
