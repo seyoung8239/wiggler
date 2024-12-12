02eecfdd4a16d3a371dce8e63ec5f836aa9a45c4
psy0909 <psy0909@midasin.com>
2024-12-12 13:00:32 +0900

    feat: reset 기능 추가

--- src/app.ts
+++ src/app.ts
@@ -1,45 +1,49 @@
+import { keyboardManager } from "./common/keyboardManager/keyboardManager";
+import { MAP_SIZE } from "./objects/game/@model/game.config";
 import { Game } from "./objects/game/game";
 
 class App {
-	canvas;
-	ctx;
+  canvas;
+  ctx;
 
-	stageWidth = 0;
-	stageHeight = 0;
-	pixelRatio = 1;
+  stageWidth = 0;
+  stageHeight = 0;
+  pixelRatio = 1;
 
-	game;
+  game;
 
-	constructor() {
-		this.canvas = document.createElement("canvas");
-		document.body.appendChild(this.canvas);
-		this.ctx = this.canvas.getContext("2d")!;
+  constructor() {
+    this.canvas = document.createElement("canvas");
+    document.body.appendChild(this.canvas);
+    this.ctx = this.canvas.getContext("2d")!;
 
-		this.game = new Game();
+    this.game = new Game();
 
-		window.addEventListener("resize", this.resize.bind(this), false);
-		this.resize();
+    window.addEventListener("resize", this.resize.bind(this), false);
+    this.resize();
 
-		requestAnimationFrame(this.animate.bind(this));
-	}
+    this.bindEvent();
 
-	resize() {
-		this.stageWidth = document.body.clientWidth;
-		this.stageHeight = document.body.clientHeight;
+    requestAnimationFrame(this.animate.bind(this));
+  }
 
-		this.canvas.width = this.stageWidth * this.pixelRatio;
-		this.canvas.height = this.stageHeight * this.pixelRatio;
-		this.ctx!.scale(this.pixelRatio, this.pixelRatio);
-	}
+  bindEvent() {
+    keyboardManager.bindKey("x", () => location.reload());
+  }
 
-	animate() {
-		window.requestAnimationFrame(this.animate.bind(this));
+  resize() {
+    this.canvas.width = MAP_SIZE.WIDTH * MAP_SIZE.UNIT;
+    this.canvas.height = MAP_SIZE.HEIGHT * MAP_SIZE.UNIT;
+  }
 
-		this.game.render(this.ctx!);
-	}
+  animate() {
+    window.requestAnimationFrame(this.animate.bind(this));
+
+    this.game.render(this.ctx!);
+  }
 }
 
 window.onload = () => {
-	new App();
-	console.log("hi");
+  new App();
+  console.log("hi");
 };
